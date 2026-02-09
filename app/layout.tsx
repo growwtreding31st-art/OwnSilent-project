6aad57505928bc95832a5150397ce479a95eb59f
Anubhab <anubhabguha1999@gmail.com>
2026-02-09 23:29:47 +0530

    Revert "UTF-8 fixes"
    This reverts commit 99f2a9bae9d26d265746629f45b9cbea5fc5a81d.

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,7 +99,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
         {/* Blocking script for instant country redirect - runs BEFORE React */}
         <script src="/country-redirect.js" />
       </head>
